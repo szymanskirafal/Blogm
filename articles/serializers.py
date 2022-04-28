327360d7eb5aff1d69d4101156c2a5aff30973eb
Rafal Szymanski <rafalszymanski@Rafals-MBP.home>
2022-04-28 21:20:38 +0200

    Add serializer view for articles

---
+++ articles/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from .models import Article
+
+
+class ArticleSerializer(serializers.ModelSerializer):
+    number_of_comments = serializers.IntegerSerializer()
+
+    class Meta:
+        model = Article
+        fields = ['id', 'title', 'content', 'number_of_comments', ]
