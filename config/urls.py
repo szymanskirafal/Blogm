c9b763e6f8225b8228fb1d1754106eddcf91db92
Rafal Szymanski <rafalszymanski@Rafals-MBP.home>
2022-04-28 15:09:22 +0200

    Add api-auth

--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('api-auth/', include('rest_framework.urls')),
 
 ]
