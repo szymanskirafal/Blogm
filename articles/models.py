7ae0f246df028ea31a9639c57a47e4cba0783a50
Rafal Szymanski <rafalszymanski@Rafals-MBP.home>
2022-04-28 13:31:06 +0200

    Add comment to articles and entries

--- articles/models.py
+++ articles/models.py
@@ -1,8 +1,13 @@
+from django.contrib.contenttypes.fields import GenericRelation
 from django.db import models
+
+from comments.models import Comment
+
 
 class Article(models.Model):
     title = models.CharField(max_length=150)
     content = models.TextField()
+    comments = GenericRelation(Comment)
 
     def __str__(self):
         return f'Article - {self.title}'
--- entries/models.py
+++ entries/models.py
@@ -1,8 +1,13 @@
+from django.contrib.contenttypes.fields import GenericRelation
 from django.db import models
+
+from comments.models import Comment
+
 
 class Entry(models.Model):
     title = models.CharField(max_length=150)
     content = models.TextField()
+    comments = GenericRelation(Comment)
 
     class Meta:
         verbose_name_plural = 'Entries'
