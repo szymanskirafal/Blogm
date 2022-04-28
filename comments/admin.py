fd9bc44213186b9301f58ed988bb88f2259e6482
Rafal Szymanski <rafalszymanski@Rafals-MBP.home>
2022-04-28 14:07:50 +0200

    Specify fields in comment admin

--- comments/admin.py
+++ comments/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models import Comment
 
-admin.site.register(Comment)
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    fields = ('body', 'content_type', 'object_id',)
