a1ba5fe833f6c99ae32360a92e635b4ecd656f5f
Rafal Szymanski <rafalszymanski@Rafals-MBP.home>
2022-04-29 10:50:47 +0200

    Add number of comments to article view

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,8 @@
-from django.contrib import admin
-from django.urls import include, path
+from rest_framework import routers
 
-urlpatterns = [
+from .views import ArticleViewSet
 
-    #path('api/', include('api.urls')),
-    
+router = routers.SimpleRouter()
+router.register(r'articles', ArticleViewSet)
 
-
-]
+urlpatterns = router.urls
--- api/views.py
+++ api/views.py
@@ -1,8 +1,11 @@
-from articles.models import Article
-from articles.serializers import ArticleSerializer
+from django.db.models import Count
+
 from rest_framework import permissions, viewsets
 
+from articles.models import Article
+from articles.serializers import ArticleSerializer
 
-class ArticlesViewSet(viewsets.ModelViewSet):
+class ArticleViewSet(viewsets.ModelViewSet):
+    queryset = Article.objects.annotate(number_of_comments=Count('comments'))
     serializer_class = ArticleSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
