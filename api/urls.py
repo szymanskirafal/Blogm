553462c06bf901ee0dc2c7bb13078f20e3ec6965
Rafal Szymanski <rafalszymanski@Rafals-MBP.home>
2022-04-29 20:42:18 +0200

    Filter comments by asset category they are set to

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,16 @@
+from django.urls import path
 from rest_framework import routers
 
-from .views import ArticleViewSet, EntryViewSet
+from .views import ArticleViewSet, CommentListAPIView, EntryViewSet
 
 router = routers.SimpleRouter()
 router.register(r'articles', ArticleViewSet)
 router.register(r'entries', EntryViewSet)
 
 urlpatterns = router.urls
+
+urlpatterns = [
+    path('comments/', CommentListAPIView.as_view(), name="comment-list"),
+]
+
+urlpatterns += router.urls
--- api/views.py
+++ api/views.py
@@ -1,10 +1,15 @@
+from django.apps import apps
+from django.contrib.contenttypes.models import ContentType
+from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Count
 
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import filters, permissions, viewsets
+from rest_framework import filters, generics, permissions, viewsets
 
 from articles.models import Article
 from articles.serializers import ArticleSerializer
+from comments.models import Comment
+from comments.serializers import CommentSerializer
 from entries.models import Entry
 from entries.serializers import EntrySerializer
 
@@ -23,3 +28,32 @@ class EntryViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     filter_backends = [filters.OrderingFilter]
     ordering_fields = ['number_of_comments']
+
+
+class CommentListAPIView(generics.ListAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+    def get_queryset(self):
+        query = Comment.objects.all()
+        asset_category = self.request.query_params.get('asset_category')
+        print('-------- asset category ', asset_category)
+        # asset_pk = self.request.query_params.get('asset_pk')
+        # print('-------- asset-pk ', asset_pk)
+        model_dict = {
+            'entry': 'entries',
+            'article': 'articles',}
+        app_name = model_dict.get(asset_category)
+        if app_name is not None:
+            asset_model = apps.get_model(app_label=app_name, model_name=asset_category)
+
+            try:
+                #asset_given = asset_model.objects.get(pk = asset_pk)
+                asset_type = ContentType.objects.get_for_model(asset_model)
+                print('------- asset_type is: ', asset_type)
+                print('------- asset_type__pk is: ', asset_type.pk)
+                query = Comment.objects.filter(content_type__pk = asset_type.id)
+
+            except ObjectDoesNotExist:
+                query = Comment.objects.all()
+        return query
