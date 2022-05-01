38bdbe143328fa1bbbe5b3e629824e119e8d6033
Rafal Szymanski <rafalszymanski@Rafals-MBP.home>
2022-05-01 17:14:40 +0200

    Change admin url

--- config/urls.py
+++ config/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('anything-but-admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('api/dj-rest-auth/', include('dj_rest_auth.urls')),
     path('api/dj-rest-auth/registration/',
