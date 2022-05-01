4db04ef61d0fb74a8679bc79932c8a38bf847e8c
Rafal Szymanski <rafalszymanski@Rafals-MBP.home>
2022-05-01 20:32:18 +0200

    Add tests and remove empty files

--- entries/tests/test_models.py
+++ entries/tests/test_models.py
@@ -1,4 +1,36 @@
+from django.db import models
+from django.contrib.contenttypes.fields import GenericRelation
+from entries.models import Entry
+
 import pytest
 
-def test_does_it_add():
-    assert add(40, 2) == 42
+class TestEntry:
+
+    def test_title_field(self):
+        field = Entry._meta.get_field('title')
+        field_type_expected = models.CharField
+        field_type_given = field.__class__
+        assert field_type_expected == field_type_given
+        field_max_length_expected = 150
+        field_max_length_given = field.max_length
+        assert field_max_length_expected == field_max_length_given
+        field_unique_expected = True
+        field_unique_given = field.unique
+        assert field_unique_expected == field_unique_given
+        com = Entry._meta.get_field('comments')
+        assert GenericRelation == com.__class__
+
+"""
+    def test_body_field(self):
+        field = Entry._meta.get_field('body')
+        field_type_expected = models.TextField
+        field_type_given = field.__class__
+        self.assertEqual(field_type_expected, field_type_given)
+        field_max_length_expected = 1000
+        field_max_length_given = field.max_length
+        self.assertEqual(field_max_length_expected, field_max_length_given)
+        field_default_expected = "some text"
+        field_default_given = field.default
+        self.assertEqual(field_default_expected, field_default_given)
+
+"""
