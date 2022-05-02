5d794f9e18b898c31d5b4c042c3862ac53d2b065
Rafal Szymanski <rafalszymanski@Rafals-MBP.home>
2022-05-02 09:10:11 +0200

    Test comment model

---
+++ comments/tests/test_models.py
@@ -0,0 +1,14 @@
+from comments.models import Comment
+
+import pytest
+
+class TestComment:
+
+    def test_body_field(self):
+        field = Comment._meta.get_field('body')
+        field_type_expected = models.CharField
+        field_type_given = field.__class__
+        assert field_type_expected == field_type_given
+        field_max_length_expected = 300
+        field_max_length_given = field.max_length
+        assert field_max_length_expected == field_max_length_given
