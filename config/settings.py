0f4717ef5af0bb44a6ca0b6910ed913054404e65
Rafal Szymanski <rafalszymanski@Rafals-MBP.home>
2022-06-09 12:44:38 +0200

    Chenage settings to run worker and beat in one Heroku dyno

--- config/settings.py
+++ config/settings.py
@@ -169,11 +169,11 @@ CELERY_RESULT_BACKEND = os.environ.get('REDIS_URL', 'redis://redis:6379/0')
 # CELERY_TASK_ALWAYS_EAGER=True
 
 CELERY_BEAT_SCHEDULE = {
-    'hello': {
-        'task': 'weather.tasks.hello',
-        'schedule': crontab()  # execute every minute
-    }
-}
+#    'hello': {
+#        'task': 'weather.tasks.hello',
+#        'schedule': crontab()  # execute every minute
+#    }
+#}
 
 # security for production
 ENVIRONMENT = os.environ.get('ENVIRONMENT')
